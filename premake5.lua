42c30e940560df3c33d3d689e886da59bcd4a6ba
Arnab Gupta <arnabgupta91996@gmail.com>
2022-03-01 21:41:13 +0530

    first commit

---
+++ premake5.lua
@@ -0,0 +1,34 @@
+-- premake5.lua
+workspace "GameGenie"
+   configurations { "Debug", "Release" }
+   platforms { "Win64" }
+
+project "Application"
+   kind "ConsoleApp"
+   language "C++"
+   targetdir "bin/%{cfg.buildcfg}"
+
+   files { "**.h", "**.cpp" }
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
+
+project "Engine"
+   kind "ConsoleApp"
+   language "C++"
+   targetdir "bin/%{cfg.buildcfg}"
+
+   files { "**.h", "**.cpp" }
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
