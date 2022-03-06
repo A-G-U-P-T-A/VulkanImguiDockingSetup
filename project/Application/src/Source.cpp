ef894888b23659bb44133bb8739774286b72a9be
Arnab Gupta <arnabgupta91996@gmail.com>
2022-03-06 17:43:07 +0530

    sub module glfw

--- project/Application/src/Source.cpp
+++ project/Application/src/Source.cpp
@@ -7,7 +7,7 @@ namespace GameGenie {
 	void init_gamegenie() {
 		cout << "TEST Application" << endl;
 		Engine engine;
-		cout << engine.Add(10, 11);
+		cout << engine.Add(10, 11) << endl;
 		system("pause");
 	}
 }
